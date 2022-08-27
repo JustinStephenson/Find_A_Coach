1f29d83c5010c04f12f47bf1c43bdb3e35726e57
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2022-08-27 15:56:05 -0400

    Adding requests

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 
 import coachesModule from '@/store/modules/coaches/index.js';
+import requestsModule from '@/store/modules/requests/index.js';
 
 export default createStore({
 	modules: {
 		coaches: coachesModule,
+		requests: requestsModule,
 	},
 	state() {
 		return {
