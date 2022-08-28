0bd6932b71c0ddc88de1d327d3e20c678c86cd85
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2022-08-28 12:06:23 -0400

    Adding not found page logic

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,4 +1,14 @@
-<template>Not Found</template>
+<template>
+	<section>
+		<base-card>
+			<h2>Page Not Found</h2>
+			<p>
+				This page could not be found - check out all out
+				<router-link to="/coaches">coaches</router-link>
+			</p>
+		</base-card>
+	</section>
+</template>
 
 <script>
 export default {};
